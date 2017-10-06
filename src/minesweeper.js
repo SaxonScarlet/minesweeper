03e6e1c2c401ac3e18642de40e1ab65d19abdd13
SaxonScarlet <saxonscarlet@users.noreply.github.com>
2017-10-06 13:16:37 +0200

    Completed day 4 of course. Minesweeper hardcoded.

---
+++ src/minesweeper.js
@@ -0,0 +1,21 @@
+// Variables
+// Represents empty row with three columns
+const blankLine = '   |   |   ';
+// Guess row - represents row when a player guesses (Hardcoded)
+const guessLine = ' 1 |   |   ';
+// Bomb row - represents row when a player reveals a bomb (Hardcoded)
+const bombLine =  '   | B |   ';
+
+// Empty board section
+console.log('This is what an empty board will look like:');
+
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// Guess and Bomb section
+console.log('This is what a board with a guess and a bomb on it would look like:');
+
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
