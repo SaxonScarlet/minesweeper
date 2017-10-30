69b66b666dcd1e4e57b7631b56528f16d539a464
SaxonScarlet <saxonscarlet@users.noreply.github.com>
2017-10-30 13:54:07 +0200

    Completed Day 14 of CodeCademy Course.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -8,7 +8,7 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     };
     board.push(row);
   };
-  
+
   return board;
 };
 
@@ -29,11 +29,54 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   while (numberOfBombsPlaced < numberOfBombs) {
     let randomRowIndex = Math.floor((Math.random() * numberOfRows));
     let randomColumnIndex = Math.floor((Math.random() * numberOfColumns));
-    board[randomRowIndex][randomColumnIndex] = 'B';
-    numberOfBombsPlaced++;
+    if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    };
   };
 
   return board;
+};
+
+const getNumberOfNeightborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+  ];
+
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1] ;
+
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns) {
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+        numberOfBombs++;
+      };
+    };
+  });
+
+  return numberOfBombs;
+};
+
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+  if (playerBoard[rowIndex][columnIndex] !== ' ') {
+    console.log('This tile has already been flipped!');
+    return;
+  } else if (bombBoard[rowIndex][columnIndex] === 'B') {
+    playerBoard[rowIndex][columnIndex] = 'B';
+  } else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeightborBombs(bombBoard, rowIndex, columnIndex);
+  };
 };
 
 const printBoard = board => {
@@ -42,12 +85,16 @@ const printBoard = board => {
 };
 
 
-let playerBoard = generatePlayerBoard(3, 4);
+let playerBoard = generatePlayerBoard(3, 3);
 
-let bombBoard = generateBombBoard(3, 4, 5);
+let bombBoard = generateBombBoard(3, 3, 5);
 
 console.log('Player Board: ');
 printBoard(playerBoard);
 
 console.log('Bomb Board: ');
-printBoard(bombBoard)
+printBoard(bombBoard);
+
+flipTile(playerBoard, bombBoard, 1, 1);
+console.log('Updated Player Board: ');
+printBoard(playerBoard);
